3729964d6809ffd572f3677d295330e2ca154f7b
davidcortesortuno <david.cortes.o@gmail.com>
2020-01-06 12:52:42 -0300

    Added test to C vtk writer

---
+++ test/test_vtk_writer_clib.py
@@ -0,0 +1,29 @@
+import oommfpy.tools as ot
+import numpy as np
+
+nx, ny, nz = 3, 3, 1
+m = np.array([0., 0., 1.,
+              0, 0, 1,
+              1, 0, 0,
+              0, 1, 1,
+              0, 1, 1,
+              0, 1, 1,
+              1, 0, 0,
+              1, 0, 0,
+              1, 0, 0
+              ])
+
+Ms = np.ones(9) * 5.
+
+xmin, xmax, ymin, ymax, zmin, zmax = 0., 3., 0., 3, 0., 1.
+grid = (np.linspace(xmin, xmax, nx + 1),
+        np.linspace(ymin, ymax, ny + 1),
+        np.linspace(zmin, zmax, nz + 1)
+        )
+
+# Write VTK file
+vtk_fname = 'test_c_vtk_writer.vtk'
+ot.clib.WriteVTK_RectilinearGrid_C(grid[0], grid[1], grid[2],
+                                   m, Ms,
+                                   nx, ny, nz,
+                                   vtk_fname)
