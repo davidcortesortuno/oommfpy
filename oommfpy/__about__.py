6ab6b7f9bcea6cab8379ac1b361cb1d04f89d3fb
davidcortesortuno <david.cortes.o@gmail.com>
2021-06-29 11:34:10 +0200

    Adding metadata to package

---
+++ oommfpy/__about__.py
@@ -0,0 +1,25 @@
+__all__ = [
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
+
+__title__ = "oommfpy"
+__summary__ = (
+    "oommfpy reads OOMMF output files and provides a class to manipulate"
+    "the data using Numpy arrays"
+)
+__uri__ = "https://github.com/davidcortesortuno/oommfpy"
+
+__version__ = "1.1b"
+
+__author__ = "David Cortés-Ortuño"
+__email__ = "d.i.cortes@uu.nl"
+
+__license__ = "BSD-2-Clause"
+__copyright__ = "Copyright 2018-2021 {}".format(__author__)
--- oommfpy/__init__.py
+++ oommfpy/__init__.py
@@ -1,3 +1,28 @@
-from .oommfpy import FieldData
-from .oommfpy import MagnetisationData
-from .oommfpy import OOMMFODTReader
+from .oommfpy import (FieldData,
+                      MagnetisationData,
+                      OOMMFODTReader
+                      )
+
+from .__about__ import (
+    __author__,
+    __copyright__,
+    __email__,
+    __license__,
+    __summary__,
+    __title__,
+    __uri__,
+    __version__,
+)
+
+
+__all__ = [
+    "oommfpy",
+    "__title__",
+    "__summary__",
+    "__uri__",
+    "__version__",
+    "__author__",
+    "__email__",
+    "__license__",
+    "__copyright__",
+]
