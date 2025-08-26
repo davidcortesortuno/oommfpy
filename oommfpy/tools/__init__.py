8e36e64391b45f69854bb2b527a3ccfcb11692f5
davidcortesortuno <david.cortes.o@gmail.com>
2025-08-26 17:13:59 -0400

    Added hsluv color option to plot tools; updated poetry deps

--- oommfpy/tools/__init__.py
+++ oommfpy/tools/__init__.py
@@ -1,5 +1,6 @@
 from .plot_tools import generate_colours
-from .plot_tools import convert_to_RGB
+from .plot_tools import convert_HLS_to_RGB
+from .plot_tools import convert_HSLUV_to_RGB
 from .plot_slices import plot_omf_slices
 from .plot_omf import plot_omf
 from .omf2vtk import omf2vtk
--- oommfpy/tools/plot_tools.py
+++ oommfpy/tools/plot_tools.py
@@ -1,37 +1,50 @@
 import colorsys
+import hsluv
 import numpy as np
 
 
 # -----------------------------------------------------------------------------
 # Utilities to generate a HSL colourmap from the magnetisation field data
+#
+def convert_HSLUV_to_RGB(hls_color):
+    """
+    From hsluv lib:
+    # hue is a float between 0 and 360, saturation and lightness are floats between 0 and 100. 
+    """
+    # NOTE: saturation is in the [2] column
+    return np.array(hsluv.hsluv_to_rgb([hls_color[0] * 180 / np.pi,
+                                        hls_color[2] * 100.,
+                                        hls_color[1] * 100.]))
 
 
-def convert_to_RGB(hls_color):
+def convert_HLS_to_RGB(hls_color):
     return np.array(colorsys.hls_to_rgb(hls_color[0] / (2 * np.pi),
                                         hls_color[1],
                                         hls_color[2]))
 
 
-def generate_colours(field_data, colour_model='rgb'):
+def generate_colours(field_data, colour_model='hls'):
     """
+    Convert field data to HLS or HSLUV colours, in RGB format
+    This can be used in imshow
+
     field_data      ::  (n, 3) array
     """
     hls = np.ones_like(field_data)
-    hls[:, 0] = np.arctan2(field_data[:, 1],
-                           field_data[:, 0]
-                           )
+    hls[:, 0] = np.arctan2(field_data[:, 1], field_data[:, 0])
     hls[:, 0][hls[:, 0] < 0] = hls[:, 0][hls[:, 0] < 0] + 2 * np.pi
     hls[:, 1] = 0.5 * (field_data[:, 2] + 1)
 
-    if colour_model == 'rgb':
-        rgbs = np.apply_along_axis(convert_to_RGB, 1, hls)
+    if colour_model == 'hls':
+        rgbs = np.apply_along_axis(convert_HLS_to_RGB, 1, hls)
         return rgbs
 
-    elif colour_model == 'hls':
-        return hls
+    elif colour_model == 'hsluv':
+        rgbs = np.apply_along_axis(convert_HSLUV_to_RGB, 1, hls)
+        return rgbs
 
     else:
-        raise Exception('Specify a valid colour model: rgb or hls')
+        raise Exception('Specify a valid colour model: hls or hsluv')
 
 
 # -----------------------------------------------------------------------------
